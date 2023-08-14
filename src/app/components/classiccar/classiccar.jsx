a27508942219d679dd92090bf555db04e05ca148
Novansyah <novansyah0001@gmail.com>
2023-08-14 13:36:51 +0700

    mobile

--- src/app/components/classiccar/classiccar.jsx
+++ src/app/components/classiccar/classiccar.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 
 const classiccar = () => {
   return (
-    <div className="w-screen h-[674px] relative overflow-hidden mb-16" id='car'>
+    <div className="w-screen h-[300px] sm:h-[674px] relative overflow-hidden mb-8 sm:mb-16" id='car'> {/* Adjusted height */}
       <div
         className="absolute top-0 left-0 w-full h-full bg-center bg-no-repeat bg-cover sm:bg-contain"
         style={{ backgroundImage: 'url("/landing_page/images/mobil.png")' }}
       >
-        <div className="h-[50px] absolute top-0 left-0 w-full flex justify-center items-center">
-          <p className="font-medium leading-relaxed tracking-widest capitalize text-7xl mt-10 text-white">Classic Car Conversion</p>
+        <div className="h-[30px] sm:h-[50px] absolute top-0 left-0 w-full flex justify-center items-center">
+          <p className="font-medium leading-relaxed tracking-widest capitalize text-4xl sm:text-7xl mt-5 sm:mt-10 text-white">
+            Classic Car Conversion
+          </p>
         </div>
-        <div className="h-[50px] absolute bottom-0 left-0 w-full flex justify-center items-center">
-          <p className="font-medium leading-relaxed tracking-widest capitalize text-7xl -mt-20 text-white">Coming Soon</p>
+        <div className="h-[30px] sm:h-[50px] absolute bottom-0 left-0 w-full flex justify-center items-center">
+          <p className="font-medium leading-relaxed tracking-widest capitalize text-4xl sm:text-7xl -mt-10 sm:-mt-20 text-white">
+            Coming Soon
+          </p>
         </div>
       </div>
     </div>
--- src/app/components/video/video.jsx
+++ src/app/components/video/video.jsx
@@ -1,26 +1,19 @@
 export default function Video() {
   return (
     <section className="relative w-full h-[calc(100vh*110/100)]" id="video">
-      <div className="font-medium leading-relaxed tracking-widest capitalize text-6xl text-center">
-      Handcrafted E-Bike
+      <div className="font-medium leading-relaxed tracking-widest capitalize text-6xl text-center mt-8 md:mt-16"> {/* Adjusted margin-top */}
+        Handcrafted E-Bike
       </div>
       <div className="absolute inset-x-0 bottom-0 h-1/2 w-full bg-[#030A0D]" />
-      <div className="flex content-center justify-center w-full h-full ">
-      
-
-
+      <div className="flex content-center justify-center w-full h-full items-center ">
         <video
           src="/landing_page/video/video_largo.mp4"
-          // type='video/mp4;codecs="avc1.42E01E, mp4a.40.2"'
           autoPlay
           loop
           controls
           muted
-          className="z-10 w-9/12 -mt-32"
+          className="z-10 w-full md:w-9/12 -mt-32"
         />
-        {/* <h1 className="absolute inset-x-0 font-bold leading-10 tracking-widest text-center capitalize -top-8 text-7xl">
-          Handcrafted e-Bike
-        </h1> */}
       </div>
     </section>
   );
