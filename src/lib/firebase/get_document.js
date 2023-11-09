347a1c192354f7bed24a84981fcabdfdf76632cf
Kenny Ekanayake <kenny.ekanayake@gmail.com>
2023-11-09 23:38:03 +0700

    start firebase migration

---
+++ src/lib/firebase/get_document.js
@@ -0,0 +1,50 @@
+import firebaseInstance from "@/lib/firebase/firebase";
+import {
+  doc,
+  getDoc,
+  getFirestore,
+  collection,
+  getDocs,
+} from "firebase/firestore";
+
+export async function getProducts({ productId = null } = {}) {
+  const firestoreInstance = getFirestore(firebaseInstance);
+
+  const productsCollection = collection(firestoreInstance, "products");
+
+  if (productId) {
+    const productReference = doc(productsCollection, productId);
+
+    const productSnapshot = await getDoc(productReference);
+
+    return [productSnapshot.data(), productSnapshot.exists()];
+  } else {
+    const productsSnapshot = await getDocs(productsCollection);
+
+    return [
+      productsSnapshot.docs.map((product) => product.data()),
+      !productsSnapshot.empty,
+    ];
+  }
+}
+
+export async function getNews({ newsId = null } = {}) {
+  const firestoreInstance = getFirestore(firebaseInstance);
+
+  const newsCollection = collection(firestoreInstance, "news");
+
+  if (newsId) {
+    const newsReference = doc(newsCollection, newsId);
+
+    const newsSnapshot = await getDoc(newsReference);
+
+    return [newsSnapshot.data(), newsSnapshot.exists()];
+  } else {
+    const allNewsSnapshot = await getDocs(newsCollection);
+
+    return [
+      allNewsSnapshot.docs.map((news) => news.data()),
+      !allNewsSnapshot.empty,
+    ];
+  }
+}
